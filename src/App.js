db25e36deb46e7221250d2b125a3768d1db83745
andrewbastian <andrew.bastian@gmail.com>
2021-04-16 13:44:48 -0700

    Editor and content displaying - working on a simple custom menubar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { menu, keymap, baseKeymap } from "./editorComponents/editorMenu.js";
+import "./editor.css";
+import "./site.css";
+
+import { EditorState } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import { Schema, DOMParser } from "prosemirror-model";
+//replace these
+import { schema } from "prosemirror-schema-basic";
+import { addListNodes } from "prosemirror-schema-list";
+import { exampleSetup } from "prosemirror-example-setup";
 
 function App() {
-  return (
-    <div className="App">
-   </div>
-  );
+    useEffect(() => {
+        const mySchema = new Schema({
+            nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+            marks: schema.spec.marks,
+        });
+        window.view = new EditorView(document.querySelector("#editor"), {
+            state: EditorState.create({
+                doc: DOMParser.fromSchema(schema).parse(
+                    document.querySelector("#content")
+                ),
+                plugins: [keymap(baseKeymap), menu],
+            }),
+        });
+    });
+    return (
+        <div className="App">
+            <div id="editor" />
+            <div id="content" />
+            Write something great!
+        </div>
+    );
 }
 
 export default App;
